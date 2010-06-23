059e61cf00c8c1646afb5e9c3f8df8ee447b1ff2
John Brezerk <brezerk@gmail.com>
2010-06-23 21:40:16 +0300

    make warning more worbose;

--- urls.py
+++ urls.py
@@ -25,7 +25,7 @@ urlpatterns = patterns('',
 
     (r'^blog-settings.html$', 'blogs.views.editBlog'),
     (r'^blog-addtopic.html$', 'blogs.views.addTopic'),
-    (r'^(?P<username>\w+)/$', 'blogs.views.viewBlog'), # this one must be last
+    (r'^(?P<username>\w+)/$', 'blogs.views.viewBlog'), # WARNING: this one MUST be last
 
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
