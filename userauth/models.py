5a3b52bd7d89994a554c0b5234e97698f519c56b
opium <redixin@red.ydk.com.ua>
2010-07-02 12:15:11 +0300

    userauth: models: almost all fields are not required anymore

--- userauth/models.py
+++ userauth/models.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
-    karma = models.IntegerField(null=True, default=0)
-    jabber = models.CharField(null=True, max_length=32)
-    website = models.CharField(null=True, max_length=32)
-    location = models.CharField(null=True, max_length=32)
-    sign = models.CharField(null=True, max_length=256)
-    photo = models.CharField(null=True, max_length=33)
+    karma = models.IntegerField(editable = False, default = 0)
+    jabber = models.CharField(blank = True, null = True, max_length = 32)
+    website = models.CharField(blank = True, null = True, max_length = 32)
+    location = models.CharField(blank = True, null = True, max_length = 32)
+    sign = models.CharField(blank = True, null = True, max_length = 256)
+    photo = models.CharField(blank = True, null = True, max_length = 33)
 
     def __unicode__(self):
         return self.user.username
