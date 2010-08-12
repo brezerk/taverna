79db5d55c318907b86f90fdad3e68752b7ce1517
Malakhov Alexey <brezerk@gmail.com>
2010-08-12 16:35:46 +0300

    update for lang and sign;

--- default_settings.py
+++ default_settings.py
@@ -43,7 +43,7 @@ TIME_ZONE = 'Europe/Kiev'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'ru_ru'
+LANGUAGE_CODE = 'ru'
 
 SITE_ID = 1
 
