f48ae5f438159b2c4d5b67b5249c027431caf461
Malakhov Alexey <brezerk@gmail.com>
2010-08-26 13:15:39 +0300

    updates;

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 by Alexey S. Malakhov <brezerk@gmail.com>
+#                       Opium <opium@jabber.com.ua>
+#
+# This file is part of Taverna
+#
+# Taverna is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Taverna is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Taverna.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.contrib.syndication.views import Feed
 from django.contrib.syndication.views import FeedDoesNotExist
 from django.shortcuts import get_object_or_404
--- userauth/openidauth.py
+++ userauth/openidauth.py
@@ -1,10 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 by Alexey S. Malakhov <brezerk@gmail.com>
+#                       Opium <opium@jabber.com.ua>
+#
+# This file is part of Taverna
+#
+# Taverna is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Taverna is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Taverna.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
 
 class OpenIDBackend:
     """Return User record if username + (some test) is valid.
-    Return None if no match.
-    """
+Return None if no match.
+"""
 
     def authenticate(self, username=None, password=None, request=None):
         try:
@@ -23,5 +43,4 @@ class OpenIDBackend:
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
-            return None
-
+            return None 
