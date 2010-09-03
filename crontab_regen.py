0f81d8e3e877ccbe5812220f75203c33e466133b
opium <redixin@red.ydk.com.ua>
2010-09-03 10:57:44 +0300

    cron: fixed race condition, and updates in one query

--- crontab_regen.py
+++ crontab_regen.py
@@ -27,12 +27,7 @@ from django.db.models import F
 
 if __name__=="__main__":
     users = Profile.objects.filter(karma__lt = settings.FORCE_REGEN['BORDER'], buryed = False)
-    for user in users:
-        user.karma += settings.FORCE_REGEN['RATE']
-        user.force = user.karma
-        user.save()
+    users.update(karma = F('karma') + 1)
 
     users = Profile.objects.filter(force__lt = F('karma'), buryed = False)
-    for user in users:
-        user.force += 1
-        user.save()
+    users.update(force = F('force') + 1)
