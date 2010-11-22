d2904effc122d4fe1e09fd3354e4b92d6f96ad07
Opium <opium@jabber.com.ua>
2010-11-22 15:50:39 +0200

    blog: tests: testStatusCodes

--- blog/tests.py
+++ blog/tests.py
@@ -18,26 +18,52 @@
 # You should have received a copy of the GNU General Public License
 # along with Taverna.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from blog.views import *
+from django.core.urlresolvers import reverse
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def setUp(self):
+        from django.contrib.auth.models import User
+        from userauth.models import Profile
+        from blog.models import Blog
+        from forum.models import Post
+        user = User.objects.create_user('test', 'test@example.com', 'secret')
+        user.save()
+        profile = Profile(user = user, visible_name = user.username)
+        profile.save()
+        blog = Blog(active = True, owner = user, name = "Tester's blog")
+        blog.save()
+        tag = Tag(name = "test tag")
+        tag.save()
+        post = Post(owner = user, title = "Title", text = "This is test text", blog = blog)
+        post.save()
+        post.tags = [tag]
+        post.save()
 
->>> 1 + 1 == 2
-True
-"""}
-
+    def testStatusCodes(self):
+        client = Client()
+        # Let's try to view @login_required pages with non logged in client.
+        # Expecting 302 status code
+        self.failUnlessEqual(client.get(reverse(blog_settings)).status_code, 302)
+        self.failUnlessEqual(client.get(reverse(post_edit, args = [1])).status_code, 302)
+        self.failUnlessEqual(client.get(reverse(post_add)).status_code, 302)
+        # Trying to log in.
+        client.login(username = "test", password = "secret")
+        # Checking @login_required pages with logged in user
+        self.failUnlessEqual(client.get(reverse(blog_settings)).status_code, 200)
+        self.failUnlessEqual(client.get(reverse(post_edit, args = [1])).status_code, 200)
+        self.failUnlessEqual(client.get(reverse(post_add)).status_code, 200)
+        # Trying all other pages:
+        self.failUnlessEqual(client.get(reverse(tags_search, args = [1])).status_code, 200)
+        self.failUnlessEqual(client.get(reverse(view, args = [1])).status_code, 200)
+        self.failUnlessEqual(client.get(reverse(index)).status_code, 200)
+        self.failUnlessEqual(client.get(reverse(firebox)).status_code, 200)
+        self.failUnlessEqual(client.get(reverse(list)).status_code, 200)
+        self.failUnlessEqual(client.get(reverse(list_public)).status_code, 200)
+        self.failUnlessEqual(client.get(reverse(list_users)).status_code, 200)
+        # TODO: check ajax votes
+        client.logout()
+        
