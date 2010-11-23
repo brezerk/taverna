0a8aabb3db61d3250dfe603d9cd1fb39456f603f
Malakhov Alexey <brezerk@gmail.com>
2010-11-23 09:37:08 +0200

    fixed: 'removed' is not a callable or an attribute of 'PostAdmin' or found in the model 'Post'.

--- forum/admin.py
+++ forum/admin.py
@@ -25,7 +25,7 @@ class ForumAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'owner', 'rating')
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'owner', 'blog', 'forum', 'removed', 'sticked', 'closed', 'solved', 'rating')
+    list_display = ('id', 'title', 'owner', 'blog', 'forum', 'sticked', 'closed', 'solved', 'rating')
 
 admin.site.register(Forum, ForumAdmin)
 admin.site.register(Post, PostAdmin)
