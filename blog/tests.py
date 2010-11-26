061b03c41e9a016fc2d7531e9c754540b0548881
Opium <opium@jabber.com.ua>
2010-11-26 12:21:42 +0200

    blog: tests: refactored

--- blog/tests.py
+++ blog/tests.py
@@ -18,52 +18,50 @@
 # You should have received a copy of the GNU General Public License
 # along with Taverna.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.test import TestCase
-from django.test.client import Client
-from blog.views import *
+from taverna.tests import BaseTest
+from views import *
 from django.core.urlresolvers import reverse
-
-class SimpleTest(TestCase):
+from blog.models import Blog
+from forum.models import Post
+ 
+class BlogTest(BaseTest):
 
     def setUp(self):
-        from django.contrib.auth.models import User
-        from userauth.models import Profile
-        from blog.models import Blog
-        from forum.models import Post
-        user = User.objects.create_user('test', 'test@example.com', 'secret')
-        user.save()
-        profile = Profile(user = user, visible_name = user.username)
-        profile.save()
-        blog = Blog(active = True, owner = user, name = "Tester's blog")
+        self.setUpProfile()
+        blog = Blog(active = True, owner = self.user, name = "Tester's blog")
         blog.save()
         tag = Tag(name = "test tag")
         tag.save()
-        post = Post(owner = user, title = "Title", text = "This is test text", blog = blog)
+        post = Post(owner = self.user, title = "Title", text = "This is test text", blog = blog)
         post.save()
         post.tags = [tag]
         post.save()
 
-    def testStatusCodes(self):
-        client = Client()
-        # Let's try to view @login_required pages with non logged in client.
-        # Expecting 302 status code
-        self.failUnlessEqual(client.get(reverse(blog_settings)).status_code, 302)
-        self.failUnlessEqual(client.get(reverse(post_edit, args = [1])).status_code, 302)
-        self.failUnlessEqual(client.get(reverse(post_add)).status_code, 302)
-        # Trying to log in.
-        client.login(username = "test", password = "secret")
-        # Checking @login_required pages with logged in user
-        self.failUnlessEqual(client.get(reverse(blog_settings)).status_code, 200)
-        self.failUnlessEqual(client.get(reverse(post_edit, args = [1])).status_code, 200)
-        self.failUnlessEqual(client.get(reverse(post_add)).status_code, 200)
-        # Trying all other pages:
-        self.failUnlessEqual(client.get(reverse(tags_search, args = [1])).status_code, 200)
-        self.failUnlessEqual(client.get(reverse(view, args = [1])).status_code, 200)
-        self.failUnlessEqual(client.get(reverse(index)).status_code, 200)
-        self.failUnlessEqual(client.get(reverse(firebox)).status_code, 200)
-        self.failUnlessEqual(client.get(reverse(list)).status_code, 200)
-        self.failUnlessEqual(client.get(reverse(list_public)).status_code, 200)
-        self.failUnlessEqual(client.get(reverse(list_users)).status_code, 200)
-        # TODO: check ajax votes
+    def testAnonymousStatusCodes(self):
+        """
+        Let's try to view @login_required pages with non logged in client.
+        Expecting 302 status codes
+        """
+        client = self.getAnonymousClient()
+        self.assertEqual(client.get(reverse(post_edit, args = [1])).status_code, 302)
+        self.assertEqual(client.get(reverse(blog_settings)).status_code, 302)
+        self.assertEqual(client.get(reverse(post_add)).status_code, 302)
+
+    def testLoggedInStatusCodes(self):
+        """
+        Checking @login_required pages with logged in user
+        """
+        client = self.getLoggedInClient()
+        self.assertEqual(client.get(reverse(post_edit, args = [1])).status_code, 200)
+        self.assertEqual(client.get(reverse(blog_settings)).status_code, 200)
+        self.assertEqual(client.get(reverse(post_add)).status_code, 200)
+
+    def testGenericStatusCodes(self):
+        client = self.getLoggedInClient()
+        self.assertEqual(client.get(reverse(tags_search, args = [1])).status_code, 200)
+        self.assertEqual(client.get(reverse(view, args = [1])).status_code, 200)
+        for v in index, firebox, list, list_public, list_users:
+            self.assertEqual(client.get(reverse(v)).status_code, 200)
         client.logout()
-        
+ 
+
