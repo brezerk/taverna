696b78393d7ed4ab46b4037b501b271238150ad9
Alexey S. Malakhov <brezerk@gmail.com>
2010-11-30 21:20:02 +0200

    Settings now django-toolbar-ready;

--- default_settings.py
+++ default_settings.py
@@ -127,6 +127,35 @@ INSTALLED_APPS = (
     'parsers',
 )
 
+# Debug toolbar
+if DEBUG:
+    MIDDLEWARE_CLASSES += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+
+    INTERNAL_IPS = ('127.0.0.1',)
+
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
+
+    DEBUG_TOOLBAR_PANELS = (
+        #'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        #'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.cache.CacheDebugPanel',
+        #'debug_toolbar.panels.logger.LoggingPanel',
+    )
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'EXCLUDE_URLS': ('/admin',),
+        'INTERCEPT_REDIRECTS': False,
+    }
+
 PARSER_ENGINES = (
     (0, 'Plain Text'),
     (1, 'BBCode'),
