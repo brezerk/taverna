1e5820593c151496defcea2db85422c6a33d5741
Opium <opium@jabber.com.ua>
2011-01-12 16:43:45 +0200

    sitemaps: quick fix for sitemaps

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -20,13 +20,14 @@
 
 from django.contrib.sitemaps import Sitemap
 from taverna.forum.models import Post
+from django.db.models import F
 
 class BlogSitemap(Sitemap):
     changefreq = "always"
     priority = 0.5
 
     def items(self):
-        return Post.objects.order_by('-created')[:10]
+        return Post.objects.filter(thread__id = F("thread__id")).order_by('-created')[:10]
 
     def lastmod(self, obj):
         return obj.created
